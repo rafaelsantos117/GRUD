acc172335f85ff8e6fbfba8e46d4190fe410f6ed
rafaelsantos117 <rafael.santos117@fatec.sp.gov.br>
2018-12-07 00:15:12 -0200

    Alterações 07/12/2018

---
+++ GRUD/GRUD/FrmClientes.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace GRUD
+{
+    partial class FrmClientes
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblDataCadastro = new System.Windows.Forms.Label();
+            this.TxtCodigo = new System.Windows.Forms.TextBox();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.TxtEndereco = new System.Windows.Forms.TextBox();
+            this.MskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.DtpDataCadastro = new System.Windows.Forms.DateTimePicker();
+            this.BtnPrimeiro = new System.Windows.Forms.Button();
+            this.BtnAnterior = new System.Windows.Forms.Button();
+            this.BtnProximo = new System.Windows.Forms.Button();
+            this.BtnUltimo = new System.Windows.Forms.Button();
+            this.BtnNovo = new System.Windows.Forms.Button();
+            this.BtnGravar = new System.Windows.Forms.Button();
+            this.BtnApagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(13, 42);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 74);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(13, 106);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(56, 13);
+            this.lblEndereco.TabIndex = 2;
+            this.lblEndereco.Text = "Endereço:";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(13, 138);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(52, 13);
+            this.lblTelefone.TabIndex = 3;
+            this.lblTelefone.Text = "Telefone:";
+            // 
+            // lblDataCadastro
+            // 
+            this.lblDataCadastro.AutoSize = true;
+            this.lblDataCadastro.Location = new System.Drawing.Point(13, 174);
+            this.lblDataCadastro.Name = "lblDataCadastro";
+            this.lblDataCadastro.Size = new System.Drawing.Size(93, 13);
+            this.lblDataCadastro.TabIndex = 4;
+            this.lblDataCadastro.Text = "Data do Cadastro:";
+            // 
+            // TxtCodigo
+            // 
+            this.TxtCodigo.Location = new System.Drawing.Point(62, 39);
+            this.TxtCodigo.Name = "TxtCodigo";
+            this.TxtCodigo.Size = new System.Drawing.Size(60, 20);
+            this.TxtCodigo.TabIndex = 5;
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(57, 71);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(287, 20);
+            this.TxtNome.TabIndex = 6;
+            // 
+            // TxtEndereco
+            // 
+            this.TxtEndereco.Location = new System.Drawing.Point(75, 103);
+            this.TxtEndereco.Name = "TxtEndereco";
+            this.TxtEndereco.Size = new System.Drawing.Size(269, 20);
+            this.TxtEndereco.TabIndex = 7;
+            // 
+            // MskTelefone
+            // 
+            this.MskTelefone.Location = new System.Drawing.Point(71, 135);
+            this.MskTelefone.Mask = "(99) 90000-0000";
+            this.MskTelefone.Name = "MskTelefone";
+            this.MskTelefone.Size = new System.Drawing.Size(89, 20);
+            this.MskTelefone.TabIndex = 8;
+            // 
+            // DtpDataCadastro
+            // 
+            this.DtpDataCadastro.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DtpDataCadastro.Location = new System.Drawing.Point(112, 168);
+            this.DtpDataCadastro.Name = "DtpDataCadastro";
+            this.DtpDataCadastro.Size = new System.Drawing.Size(95, 20);
+            this.DtpDataCadastro.TabIndex = 9;
+            // 
+            // BtnPrimeiro
+            // 
+            this.BtnPrimeiro.Location = new System.Drawing.Point(20, 222);
+            this.BtnPrimeiro.Name = "BtnPrimeiro";
+            this.BtnPrimeiro.Size = new System.Drawing.Size(75, 23);
+            this.BtnPrimeiro.TabIndex = 10;
+            this.BtnPrimeiro.Text = "<< Primeiro";
+            this.BtnPrimeiro.UseVisualStyleBackColor = true;
+            this.BtnPrimeiro.Click += new System.EventHandler(this.BtnPrimeiro_Click);
+            // 
+            // BtnAnterior
+            // 
+            this.BtnAnterior.Location = new System.Drawing.Point(101, 222);
+            this.BtnAnterior.Name = "BtnAnterior";
+            this.BtnAnterior.Size = new System.Drawing.Size(75, 23);
+            this.BtnAnterior.TabIndex = 11;
+            this.BtnAnterior.Text = "Anterior";
+            this.BtnAnterior.UseVisualStyleBackColor = true;
+            this.BtnAnterior.Click += new System.EventHandler(this.BtnAnterior_Click);
+            // 
+            // BtnProximo
+            // 
+            this.BtnProximo.Location = new System.Drawing.Point(182, 222);
+            this.BtnProximo.Name = "BtnProximo";
+            this.BtnProximo.Size = new System.Drawing.Size(75, 23);
+            this.BtnProximo.TabIndex = 12;
+            this.BtnProximo.Text = "Próximo";
+            this.BtnProximo.UseVisualStyleBackColor = true;
+            this.BtnProximo.Click += new System.EventHandler(this.BtnProximo_Click);
+            // 
+            // BtnUltimo
+            // 
+            this.BtnUltimo.Location = new System.Drawing.Point(263, 222);
+            this.BtnUltimo.Name = "BtnUltimo";
+            this.BtnUltimo.Size = new System.Drawing.Size(75, 23);
+            this.BtnUltimo.TabIndex = 13;
+            this.BtnUltimo.Text = "Último >>";
+            this.BtnUltimo.UseVisualStyleBackColor = true;
+            this.BtnUltimo.Click += new System.EventHandler(this.BtnUltimo_Click);
+            // 
+            // BtnNovo
+            // 
+            this.BtnNovo.Location = new System.Drawing.Point(61, 261);
+            this.BtnNovo.Name = "BtnNovo";
+            this.BtnNovo.Size = new System.Drawing.Size(75, 23);
+            this.BtnNovo.TabIndex = 14;
+            this.BtnNovo.Text = "Novo";
+            this.BtnNovo.UseVisualStyleBackColor = true;
+            this.BtnNovo.Click += new System.EventHandler(this.BtnNovo_Click);
+            // 
+            // BtnGravar
+            // 
+            this.BtnGravar.Location = new System.Drawing.Point(142, 261);
+            this.BtnGravar.Name = "BtnGravar";
+            this.BtnGravar.Size = new System.Drawing.Size(75, 23);
+            this.BtnGravar.TabIndex = 15;
+            this.BtnGravar.Text = "Gravar";
+            this.BtnGravar.UseVisualStyleBackColor = true;
+            this.BtnGravar.Click += new System.EventHandler(this.BtnGravar_Click);
+            // 
+            // BtnApagar
+            // 
+            this.BtnApagar.Location = new System.Drawing.Point(223, 261);
+            this.BtnApagar.Name = "BtnApagar";
+            this.BtnApagar.Size = new System.Drawing.Size(75, 23);
+            this.BtnApagar.TabIndex = 16;
+            this.BtnApagar.Text = "Apagar";
+            this.BtnApagar.UseVisualStyleBackColor = true;
+            this.BtnApagar.Click += new System.EventHandler(this.BtnApagar_Click);
+            // 
+            // FrmClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 306);
+            this.Controls.Add(this.BtnApagar);
+            this.Controls.Add(this.BtnGravar);
+            this.Controls.Add(this.BtnNovo);
+            this.Controls.Add(this.BtnUltimo);
+            this.Controls.Add(this.BtnProximo);
+            this.Controls.Add(this.BtnAnterior);
+            this.Controls.Add(this.BtnPrimeiro);
+            this.Controls.Add(this.DtpDataCadastro);
+            this.Controls.Add(this.MskTelefone);
+            this.Controls.Add(this.TxtEndereco);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.TxtCodigo);
+            this.Controls.Add(this.lblDataCadastro);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "FrmClientes";
+            this.Text = "frmClientes";
+            this.Load += new System.EventHandler(this.FrmClientes_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblDataCadastro;
+        private System.Windows.Forms.TextBox TxtCodigo;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.TextBox TxtEndereco;
+        private System.Windows.Forms.MaskedTextBox MskTelefone;
+        private System.Windows.Forms.DateTimePicker DtpDataCadastro;
+        private System.Windows.Forms.Button BtnPrimeiro;
+        private System.Windows.Forms.Button BtnAnterior;
+        private System.Windows.Forms.Button BtnProximo;
+        private System.Windows.Forms.Button BtnUltimo;
+        private System.Windows.Forms.Button BtnNovo;
+        private System.Windows.Forms.Button BtnGravar;
+        private System.Windows.Forms.Button BtnApagar;
+    }
+}
+
---
+++ GRUD/GRUD/FrmClientes.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace GRUD
+{
+    public partial class FrmClientes : Form
+    {
+        string conexao = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
+                "            D:\\Projetos\\GRUD\\GRUD\\GRUD\\Agencia.mdb";
+        OleDbConnection cn;
+        OleDbCommand cmd;
+        OleDbDataReader dr;
+        int ultimoregistro = 0;
+        int primeiroregistro = 0;
+        bool novo = false;
+
+        public FrmClientes()
+        {
+            InitializeComponent();
+        }
+
+        public int RetornaPrimeiroUltimoRegistro(bool PU)
+        {
+            int registro = 0;
+            try
+            {
+                cn = new OleDbConnection(conexao);
+                cn.Open();
+                if (PU == true)
+                {
+                    cmd = new OleDbCommand("Select MAX(CodCli) from Clientes", cn);
+                }
+                else
+                {
+                    cmd = new OleDbCommand("Select MIN(CodCli) from Clientes", cn);
+                }
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+
+                    registro = dr.GetInt32(0);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mensagem de erro: " + ex.ToString() + " - " + ex.Message, "Atenção", MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return registro;
+        }
+
+        private void RetornaDados(int cod)
+        {
+
+            try
+            {
+                cn = new OleDbConnection(conexao);
+                cn.Open();
+                if (cod == 0)
+                {
+                    cmd = new OleDbCommand("Select * from Clientes where CodCli = (Select MIN(CodCli) from Clientes)", cn);
+                }
+                else
+                {
+                    cmd = new OleDbCommand("Select * from Clientes where CodCli = " + cod, cn);
+                }
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    TxtCodigo.Text = dr.GetInt32(0).ToString();
+                    TxtNome.Text = dr.GetString(1);
+                    TxtEndereco.Text = dr.GetString(2);
+                    MskTelefone.Text = dr.GetString(3);
+                    DtpDataCadastro.Text = dr.GetDateTime(4).ToString();
+                    dr.Close();
+                    TxtNome.Focus();
+                }
+                else
+                {
+                    if (cod < ultimoregistro)
+                    {
+                        RetornaDados(cod + 1);
+                    }
+                    else
+                    {
+                        if (MessageBox.Show("Não há registros cadastrados, Deseja efetuar um cadastro?", "Atenção",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            TxtCodigo.Text = "1";
+                            TxtNome.Focus();
+                        }
+                        else
+                        {
+                            BtnPrimeiro.Enabled = false;
+                            BtnAnterior.Enabled = false;
+                            BtnProximo.Enabled = false;
+                            BtnUltimo.Enabled = false;
+                            BtnApagar.Enabled = false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mensagem de erro: " + ex.ToString() + " - " + ex.Message, "Atenção", MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void Cadastrar()
+        {
+            try
+            {
+                OleDbConnection cn = new OleDbConnection(conexao);
+                cn.Open();
+                cmd = new OleDbCommand("Insert Into Clientes(CodCli, NomeCli, EndCli, TelCli, DataCadastro) " +
+                                       "Values(@Codigo, @Nome, @Endereco, @Telefone, @DataCadastro)", cn);
+                cmd.Parameters.Add("@Codigo", OleDbType.Integer).Value = int.Parse(TxtCodigo.Text);
+                cmd.Parameters.Add("@Nome", OleDbType.VarChar).Value = TxtNome.Text;
+                cmd.Parameters.Add("@Endereco", OleDbType.VarChar).Value = TxtEndereco.Text;
+                cmd.Parameters.Add("@Telefone", OleDbType.VarChar).Value = MskTelefone.Text;
+                cmd.Parameters.Add("@DataCadastro", OleDbType.DBDate).Value = DateTime.Parse(DtpDataCadastro.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Cadastro efetuado com sucesso.", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mensagem de erro: " + ex.ToString() + " - " + ex.Message, "Atenção", MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            ultimoregistro++;
+        }
+
+        private void Atualizar()
+        {
+            try
+            {
+                OleDbConnection cn = new OleDbConnection(conexao);
+                cn.Open();
+                cmd = new OleDbCommand("Update Clientes Set NomeCli = @Nome, EndCli = @Endereco, " +
+                                       "TelCli = @Telefone, DataCadastro = @DataCadastro Where CodCli = @Codigo ", cn);
+                cmd.Parameters.Add("@Nome", OleDbType.VarChar).Value = TxtNome.Text;
+                cmd.Parameters.Add("@Endereco", OleDbType.VarChar).Value = TxtEndereco.Text;
+                cmd.Parameters.Add("@Telefone", OleDbType.VarChar).Value = MskTelefone.Text;
+                cmd.Parameters.Add("@DataCadastro", OleDbType.DBDate).Value = DateTime.Parse(DtpDataCadastro.Text);
+                cmd.Parameters.Add("@Codigo", OleDbType.Integer).Value = int.Parse(TxtCodigo.Text);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro atualizado com sucesso.", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mensagem de erro: " + ex.ToString() + " - " + ex.Message, "Atenção", MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+
+
+        private void Apagar()
+        {
+            int aux = int.Parse(TxtCodigo.Text);
+            try
+            {
+                OleDbConnection cn = new OleDbConnection(conexao);
+                cn.Open();
+                cmd = new OleDbCommand("Delete * from Clientes where CodCli = " + int.Parse(TxtCodigo.Text), cn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("O registro nº " + aux + " foi excluído com sucesso.", "OK", MessageBoxButtons.OK,
+                                 MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mensagem de erro: " + ex.ToString() + " - " + ex.Message, "Atenção", MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            RetornaDados(aux++);
+        }
+
+        private void FrmClientes_Load(object sender, EventArgs e)
+        {
+            RetornaDados(0);
+            primeiroregistro = RetornaPrimeiroUltimoRegistro(false);
+            ultimoregistro = RetornaPrimeiroUltimoRegistro(true);
+        }
+
+        private void BtnGravar_Click(object sender, EventArgs e)
+        {
+
+            if (novo == true)
+            {
+                Cadastrar();
+                ultimoregistro++;
+                TxtCodigo.Text = ultimoregistro.ToString();
+                TxtNome.Text = "";
+                TxtEndereco.Text = "";
+                MskTelefone.Text = "";
+                DtpDataCadastro.Text = "";
+                TxtNome.Focus();
+            }
+            else
+            {
+                Atualizar();
+            }
+
+        }
+
+        private void BtnAnterior_Click(object sender, EventArgs e)
+        {
+            int cod = int.Parse(TxtCodigo.Text);
+            if (int.Parse(TxtCodigo.Text) > primeiroregistro)
+            {
+                RetornaDados(cod - 1);
+            }
+            else
+            {
+                MessageBox.Show("Não há mais registros anteriores.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void BtnProximo_Click(object sender, EventArgs e)
+        {
+            int cod = int.Parse(TxtCodigo.Text);
+            if (int.Parse(TxtCodigo.Text) < ultimoregistro)
+            {
+                RetornaDados(cod + 1);
+            }
+            else if (int.Parse(TxtCodigo.Text) == ultimoregistro)
+            {
+                MessageBox.Show("Não há mais registros seguintes.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void BtnPrimeiro_Click(object sender, EventArgs e)
+        {
+            RetornaDados(primeiroregistro);
+        }
+
+        private void BtnUltimo_Click(object sender, EventArgs e)
+        {
+            RetornaDados(ultimoregistro);
+        }
+
+        private void BtnNovo_Click(object sender, EventArgs e)
+        {
+            novo = true;
+            BtnPrimeiro.Enabled = false;
+            BtnAnterior.Enabled = false;
+            BtnProximo.Enabled = false;
+            BtnUltimo.Enabled = false;
+            BtnGravar.Enabled = true;
+            BtnAnterior.Enabled = true;
+            TxtCodigo.Text = (ultimoregistro + 1).ToString();
+            TxtNome.Text = "";
+            TxtEndereco.Text = "";
+            MskTelefone.Text = "";
+            DtpDataCadastro.Text = "";
+            TxtNome.Focus();
+        }
+
+        private void BtnApagar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Você tem certeza que deseja excluir o registro nº " + TxtCodigo.Text + "? ", "Atenção",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Apagar();
+            }
+            else
+            {
+                BtnProximo.Focus();
+            }
+        }
+    }
+}
+
